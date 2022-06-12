e10c7730a111a1c14a1787c36f073d7e2f2b59c5
Zenrool <lorenzopapperini@hotmail.it>
2022-06-12 02:52:53 +0200

    Header come elemento php

---
+++ props/header.php
@@ -0,0 +1,7 @@
+<header>
+    <div class="header__logo d-flex align-items-center h-100">
+        <div class="h-75 ml-1">
+            <img src="./img/logo.png" alt="logo" />
+        </div>
+    </div>
+</header>
